c4fd609247ef481495a490f8e0257052a9f96a49
Juhasz Evelin <juhaaszevelin@gmail.com>
2021-04-29 10:22:29 +0200

    all tests passed

--- src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
+++ src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
@@ -1,5 +1,29 @@
 package hu.nive.ujratervezes.adddigits;
 
 public class AddDigits {
-    //todo: implement your solution here
+    public int addDigits(String input) {
+
+        if (input == null || input.length() == 0) {
+            return -1;
+        }
+
+        String numbers = input
+                .replaceAll("[^\\d]", " ")
+                .trim()
+                .replaceAll(" +", "");
+
+        if (numbers.length() == 0) {
+            return 0;
+        }
+
+        int sum = 0;
+
+        for (int i = 0; i < numbers.length(); i++) {
+            String num = numbers.substring(i, i + 1);
+            sum += Integer.parseInt(num);
+        }
+
+        return sum;
+    }
+
 }
